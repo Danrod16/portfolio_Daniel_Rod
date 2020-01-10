4978753305aeb8719ecee8f925279f5756baee80
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-01-10 15:15:55 +0100

    FIX: scroll botton

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["I help change makers find and tell their story", "I also build trendy web applications", "I'm some kind of self-learner junky", "I love Tacos!"],
+    strings: ["I help change makers tell their story", "I also build trendy web applications", "I'm some kind of self-learner junky", "I love Tacos!"],
     typeSpeed: 30,
     loop: true
   });
